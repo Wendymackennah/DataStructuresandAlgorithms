39e69aa1a439db6e1a3128efd57855b6a9cc0e08
Wendymackennah <wendymackennah5@gmail.com>
2024-11-18 21:00:42 +0300

    java

---
+++ wendy/Thingstoknowinanylanguage/basicinput.java
@@ -0,0 +1,28 @@
+// importing the necessary library
+// import java.util.scannner
+
+// Input and output operations enable Java programs to interact with users, making it possible to create user-friendly applications with graphical interfaces or command-line interfaces. Input allows programs to obtain data from various sources, such as users, files, or external devices. This data can be used for processing, analysis, or storage. Whereas, output enables programs to present results or information to users in a readable and meaningful format. This is essential for displaying data, reports, or graphical visualizations.
+
+// Java offers two input methods, Scanner and BufferedReader, to cater to diverse programming needs. Scanner simplifies console input for common use cases, providing easy-to-use methods for various data types. BufferedReader, on the other hand, offers greater control and efficiency, making it suitable for complex input scenarios and handling large volumes of data, such as reading from files or network streams. 
+
+// Including Libraries
+// Java is a versatile language, that relies on libraries to access various functionalities. To perform tasks like input and output, we include specific libraries at the beginning of our code. One such essential library is java.util, which includes the Scanner class. The Scanner class is a workhorse for handling user input, allowing you to effortlessly read data from the keyboard or other sources.
+
+
+// The generic skeleton of a Java program consists of two main components: the import statements and the main method. After importing the necessary libraries, you declare the main method using public static void main(String[] args) { /* Your code here */ }. This serves as the entry point for your program.
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        // Your code here
+    }
+}
+// Output with System.out.println
+
+// To display output in Java, you'll commonly use the System.out.println method. There's no need to specify a namespace as in C++. For instance, System.out.println("Hey, Striver!"); will print "Hey, Striver!" to the console. You enclose the text you want to display within double quotation marks.
+
+// Code:
+
+// The println method automatically adds a newline character at the end of the printed text. So, each time you use println, the output will be on a new line.
+
